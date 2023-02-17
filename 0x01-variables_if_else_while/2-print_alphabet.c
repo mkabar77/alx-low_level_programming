c250d29740dc99ec590438765b5b0163cec5084f
mkabar77 <almahak77@gmail.com>
2023-02-17 14:56:41 -0800

    Last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ */
+int main()
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+		if(alp == 'c' || alp == 'y' || alp == 'o')
+			continue;
+		putchar(alp);
+	}
+
+
+	putchar('\n');
+		return 0;
+}
