312080cc73bde6064db66bb7850d91cfc08168c7
mkabar77 <almahak77@gmail.com>
2023-02-23 05:46:44 -0800

    Multiplication

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,29 @@
 #include "main.h"
-
+#include <stdio.h>
+/**
+ * times_table - multiplication of 9
+ * main - main functions
+ *
+ * Return: Always 9
+ */
 
 void times_table(void)
 {
+	int a, b, x, mul, chk;
 
+	for (a = 0; a <= 9; a++)
+	{
+		for ((b = 0; x = 1 ; b <= 9); (b++, x++);)
+			{
+			mul = a * b;
+			chk = a * x;
+		if (chk > 9 && b != 9)
+			printf("%d, ", mul);
+				else if (b == 9)
+					printf("%d", mul);
+				else
+					printf("%d,  ", mul);
+			}
+		printf("\n");
+	}
 }
