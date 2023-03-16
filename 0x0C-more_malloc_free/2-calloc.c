770ffef3d5af41d2d26deeb742c41fa0759d2884
mkabar77 <almahak77@yahoo.com>
2023-03-16 16:40:34 -0700

    Stressed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,6 @@
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char k, unsigned int n)
-
 {
 	unsigned int i;
 
@@ -20,4 +19,25 @@ char *_memset(char *s, char k, unsigned int n)
 		s[i] = k;
 	}
 	return (s);
+}
+
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
