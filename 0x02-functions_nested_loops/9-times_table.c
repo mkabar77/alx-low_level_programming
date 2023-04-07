69c03a207ca86cbead977311771ad67085e1b42d
mkabar77 <almahak77@gmail.com>
2023-04-07 03:42:04 -0700

    Sum

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for ((b = 0; x = 1 ; b <= 9); (b++, x++);)
+		for (((b = 0) x = 1 ; b <= 9) ; b++, x++;)
 			{
 			mul = a * b;
 			chk = a * x;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Readme
+A README.md file, at the root of the folder of the project is mandatory
