8fae69429d0a7600e30fda0d51a3ee154eba78fd
mkabar77 <almahak77@gmail.com>
2023-04-08 09:53:49 -0700

    Binary to number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of number
+ * @n: number represented in binary
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int j;
+	int binary[32];
+
+	while (n > 0)
+	{
+		binary[i] = n % 2 + '0';
+		n /= 2;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		putchar(binary[j] + 0);
+	}
+}
+/**
+ * int main()
+{
+	int num = 42;
+	print_binary(n);
+	return 0;
+}
+*/
