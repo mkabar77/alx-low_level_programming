28c6cc92d30c87973b66d00b7930152c9966b110
almahak77 <almahak77@gmail.com>
2023-05-08 12:42:37 -0700

    I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,53 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 
 /**
- * read_textfile - this read text
- * @filename: text file
- * @letters: alphabet to be read
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the name of the file to be read
+ * @letters: number of letters that should be read and printed
  *
- * Return: num_written, if erros return 0
+ * Return: actual number of letters that were read and printed,
+ *         0 if file cannot be opened or read, or if write fails
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, bytes_read, bytes_written;
+	char *buffer;
+
 	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
+		free(buffer);
 		return (0);
 	}
 
-	FILE *file;
-
-	file = fopen(filename, "r");
-
-	if (file == NULL)
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
 	{
+		free(buffer);
+		close(fd);
 		return (0);
 	}
 
-	char buffer[letters];
-	size_t num_read;
-
-	num_read = fread(buffer, sizeof(char), letters, file);
-
-	if (num_read == 0)
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		fclose(file);
+		free(buffer);
+		close(fd);
 		return (0);
 	}
 
-	ssize_t num_written;
+	free(buffer);
+	close(fd);
 
-	num_written = fwrite(buffer, sizeof(char), num_read, stdout);
-	fclose(file);
-
-	if (num_written != num_read)
-	{
-		return (0);
-	}
-
-	return (num_written);
+	return (bytes_written);
 }
---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,161 @@
+#include <elf.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * display_error - display an error message to stderr and exit with status 98
+ * @msg: the error message
+ */
+void display_error(char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(98);
+}
+
+/**
+ * main - entry point
+ * @argc: the number of command-line arguments
+ * @argv: an array of command-line argument strings
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char **argv)
+{
+	int fd, i;
+	Elf32_Ehdr header;
+	char buf[BUF_SIZE];
+
+	if (argc != 2)
+		display_error("Usage: elf_header elf_filename");
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		display_error("Error: Cannot read file");
+
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+		display_error("Error: Cannot read ELF header");
+
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+			header.e_ident[EI_MAG1] != ELFMAG1 ||
+			header.e_ident[EI_MAG2] != ELFMAG2 ||
+			header.e_ident[EI_MAG3] != ELFMAG3)
+		display_error("Error: Not an ELF file");
+
+	printf("ELF Header:\n");
+
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header.e_ident[i]);
+	printf("\n");
+
+	printf("  Class:                             ");
+	switch (header.e_ident[EI_CLASS])
+	{
+		case ELFCLASSNONE:
+			printf("none\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header.e_ident[EI_CLASS]);
+			break;
+	}
+
+	printf("  Data:                              ");
+	switch (header.e_ident[EI_DATA])
+	{
+		case ELFDATANONE:
+			printf("none\n");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header.e_ident[EI_DATA]);
+			break;
+	}
+
+	printf("  Version:                           %d\n",
+		header.e_ident[EI_VERSION]);
+
+	printf("  OS/ABI:                            ");
+	switch (header.e_ident[EI_OSABI])
+	{
+		case ELFOSABI_NONE:
+				printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD\n");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - Linux\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris\n");
+			break;
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX\n");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD\n");
+			break;
+		case ELFOSABI_TRU64:
+			printf("UNIX - TRU64\n");
+			break;
+		case ELFOSABI_ARM:
+			printf("ARM\n");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf("Standalone App\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header.e_ident[EI_OSABI]);
+			break;
+	}
+
+	printf("  ABI Version:                       %d\n",
+		header.e_ident[EI_ABIVERSION]);
+
+	printf("  Type:                              ");
+	switch (header.e_type)
+	{
+		case ET_NONE:
+			printf("NONE (None)\n");
+			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_DYN:
+			printf("DYN (Shared object file)\n");
+			break;
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header.e_type);
+			break;
+	}
+	printf(" Entry point address:	%#|x\n",(unsigned long)header.e_entry);
+			close(fd);
+			return(0);
+}
+
